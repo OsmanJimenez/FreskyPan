efdbc97bb8fb2b98ca67c81ca68eb7b927222a6a
josed9611 <61365152+josed9611@users.noreply.github.com>
2020-05-08 18:11:01 -0500

    validacion de campo
    validacion

--- backend/Ventas_Modificar.php
+++ backend/Ventas_Modificar.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <title>Modificar Ventas</title>
-   
+
   <!-- Font-->
   <link rel="stylesheet" type="text/css" href="css/roboto-font.css">
   <link rel="stylesheet" type="text/css" href="fonts/font-awesome-5/css/fontawesome-all.min.css">
@@ -76,7 +76,7 @@
           <div class="card-body">
             <!-- Add Example -->
             <form action="../basededatos/editarv.php" method="POST">
-              
+
 
 
               <div class="card-header py-3">
@@ -94,45 +94,45 @@ $query="Select * from venta,produccion,venta_produccion where venta.ID_VENTA='$m
         venta.ID_VENTA=venta_produccion.FK_ID_VENTA and
         venta_produccion.FK_ID_PRODUCCION=produccion.ID_PRODUCCION";
 $result=mysqli_query($conn,$query);
-      
-      $fila=mysqli_fetch_array($result); 
+
+      $fila=mysqli_fetch_array($result);
         $idv = $fila['ID_VENTA'];
         $idp = $fila['FK_ID_PRODUCCION'];
         $can=$fila['cantidad'];
         $fecha=$fila['fecha'];
-      
+
         ?>
                 <div class="form-group col-md-6">
                 <label for="inputCantidad">ID Venta</label>
-                  <input type="text" name="cod" class="form-control" id="inputCantidad1" placeholder="" value="<?php echo $idv; ?>">
+                  <input type="number" name="cod" class="form-control" id="inputCantidad1"onkeypress="return id_vent(event)" oninput="maxlengthNumber(this)" onpaste="return false" placeholder="" value="<?php echo $idv; ?>">
                   <div class="space-small"></div>
                   <label for="inputCantidad">ID Producción</label>
                   <input type="number" name="pro" class="form-control" id="pro" readonly="" placeholder="" value="<?php echo $idp; ?>">
                   <label for="inputCantidad">Fecha</label>
                   <input type="date" name="fec" class="form-control" id="inputCantidad3" placeholder="" value="<?php echo $fec; ?>">
                   <label for="inputCantidad">Cantidad</label>
-                  <input type="number" name="can" class="form-control" id="inputCantidad2" placeholder="" value="<?php echo $can; ?>">
+                  <input type="number" name="can" class="form-control" id="inputCantidad2" onkeypress="return Cant_p(event)" oninput="maxlengthNumber(this)" onpaste="return false" placeholder="" value="<?php echo $can; ?>">
                   <div class="space-small"></div>
                   <button type="submit" class="btn btn-primary  ">Agregar</button>
                 </div>
-              </div>  
-              
-              
-
-              
               </div>
 
-              
-            
-              
+
+
+
+              </div>
+
+
+
+
           <input type="hidden" name="id" id="prueba" readonly="">
 
-          
-          
-              
+
+
+
               </form>
-              
-        
+
+
 
           </div>
 
@@ -140,11 +140,11 @@ $result=mysqli_query($conn,$query);
 
 
       </div>
-          
-  </div> 
+
+  </div>
 
           <!--End  Add Example -->
-        
+
 
 </div>
     <!-- /.container-fluid -->
@@ -169,7 +169,7 @@ $result=mysqli_query($conn,$query);
         $(".pr").click(function(){
  var flag=1;
             var valores="";
- 
+
             // Obtenemos todos los valores contenidos en los <td> de la fila
             // seleccionada
             $(this).parents("tr").find("td").each(function(){
@@ -186,8 +186,8 @@ $result=mysqli_query($conn,$query);
                 console.log(flag);
             });
 
-           
-          
+
+
         });
     });
     </script>
@@ -196,7 +196,7 @@ $result=mysqli_query($conn,$query);
         $(".pr2").click(function(){
  var flag=1;
             var valores="";
- 
+
             // Obtenemos todos los valores contenidos en los <td> de la fila
             // seleccionada
             $(this).parents("tr").find("td").each(function(){
@@ -211,11 +211,64 @@ $result=mysqli_query($conn,$query);
                 console.log(flag);
             });
 
-           
-          
+
+
         });
     });
     </script>
+
+    <!-- validacion de longitud de campo numerico -->
+        <script>
+      function maxlengthNumber(ob){
+        console.log(ob.value);
+
+        if(ob.value.length > ob.maxLength){
+
+          ob.value = ob.value.slice(0,ob.maxLength);
+        }
+      }
+
+
+    </script>
+    <script type="text/javascript">
+  function id_vent(evento){
+
+      key = evento.keyCode || evento.which;
+       teclado = String.fromCharCode(key).toLocaleLowerCase();
+          cod = "1234567890";
+            especiales = "37-38-46";
+
+            teclado_especial = false;
+            for (var i in especiales) {
+                if (key == especiales[i]) {
+                    teclado_especial = true; break;
+                }
+            }
+            if (cod.indexOf(teclado) == -1 && !teclado_especial) {
+                return false;
+            }
+  }
+ </script>
+
+      <script type="text/javascript">
+    function Cant_p(evento){
+
+        key = evento.keyCode || evento.which;
+         teclado = String.fromCharCode(key).toLocaleLowerCase();
+            can = "1234567890";
+              especiales = "37-38-46";
+
+              teclado_especial = false;
+              for (var i in especiales) {
+                  if (key == especiales[i]) {
+                      teclado_especial = true; break;
+                  }
+              }
+              if (can.indexOf(teclado) == -1 && !teclado_especial) {
+                  return false;
+              }
+    }
+   </script>
     <!-- Core plugin JavaScript-->
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
