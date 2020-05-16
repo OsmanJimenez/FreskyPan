45e092b659151612847c1407b7d901b09bc6fee7
Osman Armando Jimenez Cortes <33326926+OsmanJimenez@users.noreply.github.com>
2020-05-15 20:10:52 -0500

    Fix DarkMode Calendar

--- backend/calendario/index.php
+++ backend/calendario/index.php
@@ -75,51 +75,42 @@ if (isset($_POST['from'])) {
 <html lang="es">
 
 <head>
-    <meta charset="utf-8">
-    <title>Calendario</title>
-    <link rel="stylesheet" type="text/css" href="css/calendar.css">
+  <meta charset="utf-8">
+  <title>Calendario</title>
+  <link rel="stylesheet" type="text/css" href="css/calendar.css">
+  <meta http-equiv="Expires" content="0">
+  <meta http-equiv="Last-Modified" content="0">
+  <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+  <meta http-equiv="Pragma" content="no-cache">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <script type="text/javascript" src="js/es-ES.js"></script>
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/moment.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap-datetimepicker.js"></script>
+  <link rel="stylesheet" href="css/bootstrap-datetimepicker.min.css" />
 
-    <link rel="stylesheet" href="css/font-awesome.min.css" >
-    <script type="text/javascript" src="js/es-ES.js"></script>
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="js/moment.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap-datetimepicker.js"></script>
-    <link rel="stylesheet" href="css/bootstrap-datetimepicker.min.css" />
-    
-   
   <!-- Font-->
   <link rel="stylesheet" type="text/css" href="../css/roboto-font.css">
   <link rel="stylesheet" type="text/css" href="../fonts/font-awesome-5/css/fontawesome-all.min.css">
-  <!-- Main Style Css -->
-
-
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
 
   <!-- Custom favicon for this template-->
   <link rel="icon" type="image/png" href="../../favicon.png" />
 
-  <title>Pedidos - ERP</title>
-
   <!-- Custom fonts for this template-->
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link
-    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
   <link href="../css/sb-admin-2.css" rel="stylesheet">
 
   <!-- Custom calendar -->
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
-    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
-
-
+  <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+  <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+  <?php
+  require('../Style.php');
+  ?>
 </head>
 
 <body id="page-top" style="background: white;">
